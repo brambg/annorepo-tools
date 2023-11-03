e552b19de053d48c5431ff449df74b3da00dbdf7
Bram Buitendijk <bram.buitendijk@di.huc.knaw.nl>
2023-11-03 15:56:05 +0100

    add integration tests for anonymous read access

--- performance-tester/src/main/kotlin/nl/knaw/huc/di/annorepo/tools/PerformanceTester.kt
+++ performance-tester/src/main/kotlin/nl/knaw/huc/di/annorepo/tools/PerformanceTester.kt
@@ -1,86 +1,87 @@
 package nl.knaw.huc.di.annorepo.tools
-
-import java.io.File
-import java.net.URI
-import kotlin.system.measureTimeMillis
-import com.fasterxml.jackson.core.type.TypeReference
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.module.kotlin.kotlinModule
-import kotlinx.coroutines.flow.count
-import kotlinx.coroutines.runBlocking
+//
+//import java.io.File
+//import java.net.URI
+//import kotlin.system.measureTimeMillis
+//import com.fasterxml.jackson.core.type.TypeReference
+//import com.fasterxml.jackson.databind.ObjectMapper
+//import com.fasterxml.jackson.module.kotlin.kotlinModule
+//import kotlinx.coroutines.flow.count
+//import kotlinx.coroutines.runBlocking
 import org.slf4j.LoggerFactory
-import nl.knaw.huc.annorepo.api.WebAnnotationAsMap
-import nl.knaw.huc.annorepo.client.AnnoRepoClient
-
+//import nl.knaw.huc.annorepo.api.WebAnnotationAsMap
+//import nl.knaw.huc.annorepo.client.AnnoRepoClient
+//
 object PerformanceTester {
     private val log = LoggerFactory.getLogger(PerformanceTester::class.java)
     private val restContainer = "rest-test-container"
     private val grpcContainer = "grpc-test-container"
-    private val mapper = ObjectMapper().apply { registerModules(kotlinModule()) }
-    private val path = "/Users/bram/workspaces/globalise/globalise-tools/out/px_textline_annotations.json"
-
+//    private val mapper = ObjectMapper().apply { registerModules(kotlinModule()) }
+//    private val path = "/Users/bram/workspaces/globalise/globalise-tools/out/px_textline_annotations.json"
+//
     @JvmStatic
     fun main(args: Array<String>) {
-        runBlocking {
-            val annoRepoClient = AnnoRepoClient(serverURI = URI("http://localhost:2023"), apiKey = "dummy-api-key")
-            val annotations: List<WebAnnotationAsMap> =
-                loadAnnotations(path)
-            val total = annotations.size
-            println("uploading $total annotations:")
-
-            val restElapsed = measureTimeMillis {
-                uploadUsingRest(annoRepoClient, restContainer, annotations)
-            }
-            val restAvg: Float = restElapsed / (total).toFloat()
-            println("using rest: ${(restElapsed).toFloat() / 1000} seconds = $restAvg milliseconds/annotation")
-
-            val grpcElapsed = measureTimeMillis {
-                uploadUsingGrpc(annoRepoClient, grpcContainer, annotations)
-            }
-            val grpcAvg: Float = grpcElapsed / (total).toFloat()
-            println("using gRPC: ${(grpcElapsed).toFloat() / 1000} seconds = $grpcAvg milliseconds/annotation")
-        }
+        TODO()
+//        runBlocking {
+//            val annoRepoClient = AnnoRepoClient(serverURI = URI("http://localhost:2023"), apiKey = "dummy-api-key")
+//            val annotations: List<WebAnnotationAsMap> =
+//                loadAnnotations(path)
+//            val total = annotations.size
+//            println("uploading $total annotations:")
+//
+//            val restElapsed = measureTimeMillis {
+//                uploadUsingRest(annoRepoClient, restContainer, annotations)
+//            }
+//            val restAvg: Float = restElapsed / (total).toFloat()
+//            println("using rest: ${(restElapsed).toFloat() / 1000} seconds = $restAvg milliseconds/annotation")
+//
+//            val grpcElapsed = measureTimeMillis {
+//                uploadUsingGrpc(annoRepoClient, grpcContainer, annotations)
+//            }
+//            val grpcAvg: Float = grpcElapsed / (total).toFloat()
+//            println("using gRPC: ${(grpcElapsed).toFloat() / 1000} seconds = $grpcAvg milliseconds/annotation")
+//        }
     }
-
-    private fun loadAnnotations(path: String): List<WebAnnotationAsMap> {
-        val jsonString = File(path).readText(Charsets.UTF_8)
-        val valueTypeRef = object : TypeReference<List<WebAnnotationAsMap>>() {}
-        return mapper.readValue(jsonString, valueTypeRef)
-    }
-
-    private fun uploadUsingRest(
-        annoRepoClient: AnnoRepoClient,
-        containerName: String,
-        annotations: List<WebAnnotationAsMap>
-    ) {
-        annoRepoClient.createNewContainer(containerName)
-        val result = annoRepoClient.batchUpload(containerName, annotations)
-        println("result size=${result.getOrNull()?.annotationData?.size}")
-//        log.info("result={}", result)
-    }
-
-    private suspend fun uploadUsingGrpc(
-        annoRepoClient: AnnoRepoClient,
-        containerName: String,
-        annotations: List<WebAnnotationAsMap>
-    ) {
-        annoRepoClient.createNewContainer(containerName)
-        annoRepoClient.usingGrpc { grpc ->
-            val resultFlow = grpc.addContainerAnnotation(containerName = containerName, annotations = annotations)
-            println("result size=${resultFlow.count()}")
-        }
-    }
-
-    private fun AnnoRepoClient.createNewContainer(containerName: String) {
-        getContainer(containerName).fold(
-            {},
-            { result ->
-                deleteContainer(containerName = containerName, eTag = result.eTag, force = true)
-            }
-        )
-        createContainer(preferredName = containerName).fold(
-            { error -> throw RuntimeException(error.message) },
-            { result -> println("created ${result.location}") }
-        )
-    }
+//
+//    private fun loadAnnotations(path: String): List<WebAnnotationAsMap> {
+//        val jsonString = File(path).readText(Charsets.UTF_8)
+//        val valueTypeRef = object : TypeReference<List<WebAnnotationAsMap>>() {}
+//        return mapper.readValue(jsonString, valueTypeRef)
+//    }
+//
+//    private fun uploadUsingRest(
+//        annoRepoClient: AnnoRepoClient,
+//        containerName: String,
+//        annotations: List<WebAnnotationAsMap>
+//    ) {
+//        annoRepoClient.createNewContainer(containerName)
+//        val result = annoRepoClient.batchUpload(containerName, annotations)
+//        println("result size=${result.getOrNull()?.annotationData?.size}")
+////        log.info("result={}", result)
+//    }
+//
+//    private suspend fun uploadUsingGrpc(
+//        annoRepoClient: AnnoRepoClient,
+//        containerName: String,
+//        annotations: List<WebAnnotationAsMap>
+//    ) {
+//        annoRepoClient.createNewContainer(containerName)
+//        annoRepoClient.usingGrpc { grpc ->
+//            val resultFlow = grpc.addContainerAnnotation(containerName = containerName, annotations = annotations)
+//            println("result size=${resultFlow.count()}")
+//        }
+//    }
+//
+//    private fun AnnoRepoClient.createNewContainer(containerName: String) {
+//        getContainer(containerName).fold(
+//            {},
+//            { result ->
+//                deleteContainer(containerName = containerName, eTag = result.eTag, force = true)
+//            }
+//        )
+//        createContainer(preferredName = containerName).fold(
+//            { error -> throw RuntimeException(error.message) },
+//            { result -> println("created ${result.location}") }
+//        )
+//    }
 }
--- performance-tester/src/test/kotlin/nl/knaw/huc/di/annorepo/tools/PerformanceTesterTest.kt
+++ performance-tester/src/test/kotlin/nl/knaw/huc/di/annorepo/tools/PerformanceTesterTest.kt
@@ -1,12 +1,109 @@
 package nl.knaw.huc.di.annorepo.tools
 
+import java.net.URI
+import kotlin.test.fail
 import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
+import arrow.core.raise.either
+import org.assertj.core.api.Assertions.assertThat
+import org.slf4j.LoggerFactory
+import nl.knaw.huc.annorepo.api.WebAnnotation
+import nl.knaw.huc.annorepo.client.AnnoRepoClient
+import nl.knaw.huc.annorepo.client.RequestError
 
+@Disabled
 class PerformanceTesterTest {
-    @Disabled
+    private val log = LoggerFactory.getLogger(PerformanceTesterTest::class.java)
+    private val annorepoUrl = "http://localhost:2023"
+
+    private val rootClient = AnnoRepoClient(
+        serverURI = URI(annorepoUrl),
+        apiKey = "root"
+    )
+    private val userClient = AnnoRepoClient(
+        serverURI = URI(annorepoUrl),
+        apiKey = "user"
+    )
+    private val anonymousClient = AnnoRepoClient(
+        serverURI = URI(annorepoUrl)
+    )
+
     @Test
-    fun main() {
-        PerformanceTester.main(arrayOf())
+    fun `on a container with authorization on and read-only access for anonymous users, reading should be allowed for any user`() {
+        val readableContainerName = "container-with-read-access-for-anonymous-users"
+        either {
+            rootClient.createNewContainer(containerName = readableContainerName, readOnlyForAnonymousUsers = true)
+            val annotation = WebAnnotation.Builder().withBody("my-body").withTarget("my-target").build()
+            val addResponse = rootClient.createAnnotation(readableContainerName, annotation).bind()
+            log.info("annotation location: {}", addResponse.location)
+
+            val rootReadResponse = rootClient.getAnnotation(addResponse.containerName, addResponse.annotationName)
+            assertThat(rootReadResponse.isRight())
+                .withFailMessage { "annotation should be readable by root user" }
+                .isTrue()
+
+            val userReadResponse = userClient.getAnnotation(addResponse.containerName, addResponse.annotationName)
+            assertThat(userReadResponse.isRight())
+                .withFailMessage { "annotation should be readable by authorized user" }
+                .isTrue()
+
+            val anonymousReadResponse =
+                anonymousClient.getAnnotation(addResponse.containerName, addResponse.annotationName)
+            assertThat(anonymousReadResponse.isRight())
+                .withFailMessage { "annotation should be readable by anonymous user" }
+                .isTrue()
+
+        }.mapLeft {
+            log.error("error: {}", it.message)
+            fail()
+        }
     }
+
+    @Test
+    fun `on a container with authorization on and no read-only access for anonymous users, reading should be allowed authorized users only`() {
+        val protectedContainerName = "protected-container"
+        either {
+            rootClient.createNewContainer(containerName = protectedContainerName, readOnlyForAnonymousUsers = false)
+            val annotation = WebAnnotation.Builder().withBody("my-body").withTarget("my-target").build()
+            val addResponse = rootClient.createAnnotation(protectedContainerName, annotation).bind()
+            log.info("annotation location: {}", addResponse.location)
+
+            val rootReadResponse = rootClient.getAnnotation(addResponse.containerName, addResponse.annotationName)
+            log.info("response={}", rootReadResponse)
+            assertThat(rootReadResponse.isRight())
+                .withFailMessage { "annotation should be readable by root user" }
+                .isTrue()
+
+            val userReadResponse = userClient.getAnnotation(addResponse.containerName, addResponse.annotationName)
+            log.info("response={}", userReadResponse)
+            assertThat((userReadResponse.isLeft() and (userReadResponse.leftOrNull() is RequestError.NotAuthorized)))
+                .withFailMessage { "annotation should not be readable by other authorized user" }
+                .isTrue()
+
+            val anonymousReadResponse =
+                anonymousClient.getAnnotation(addResponse.containerName, addResponse.annotationName)
+            log.info("response={}", anonymousReadResponse)
+            assertThat((anonymousReadResponse.isLeft() and (anonymousReadResponse.leftOrNull() is RequestError.NotAuthorized)))
+                .withFailMessage { "annotation should not be readable by anonymous user" }
+                .isTrue()
+
+        }.mapLeft {
+            log.error("error: {}", it.message)
+            fail()
+        }
+    }
+
+    private fun AnnoRepoClient.createNewContainer(containerName: String, readOnlyForAnonymousUsers: Boolean) {
+        getContainer(containerName).fold(
+            {},
+            { result ->
+                deleteContainer(containerName = containerName, eTag = result.eTag, force = true)
+            }
+        )
+        createContainer(preferredName = containerName, readOnlyForAnonymousUsers = readOnlyForAnonymousUsers).fold(
+            { error -> throw RuntimeException(error.message) },
+            { result -> println("created ${result.location}") }
+        )
+    }
+
 }
